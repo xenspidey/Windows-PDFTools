b17446bc2cfebfe2fb9137d41cd429b782c45091
bcalendine <bcalendine>
2017-03-03 16:09:51 -0500

    Added merge capability

--- PDF.py
+++ PDF.py
@@ -1,24 +1,42 @@
 """Merge multiple PDFs
 """
-from PyPDF2 import PdfFileMerger, PdfFileReader
+from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 from tkinter import *
-from tkinter.filedialog import askopenfilenames, asksaveasfilename
+from tkinter.filedialog import askopenfilenames, askopenfilename, asksaveasfilename
 
 
 def merge():
     '''Merge function'''
     def move_up(listbox):
         '''move file up'''
-        print(listbox.curselection())
-        items = listbox.curselection()
-        for item in items:
-            item.index()
+        poslist = listbox.curselection()
+        if not poslist:
+            return
+        for pos in poslist:
+            if pos == 0:
+                return
+            text = listbox.get(pos)
+            listbox.delete(pos)
+            listbox.insert(pos-1, text)
+            listbox.selection_set(pos-1)
 
     def move_down(listbox):
         '''move file down'''
-        print('move down')
+        poslist = listbox.curselection()
+        if not poslist:
+            return
+        for pos in reversed(poslist):
+            print(pos, len(listbox.get(0, END)))
+            if pos+1 == len(listbox.get(0, END)):
+                return
+            text = listbox.get(pos)
+            listbox.delete(pos)
+            listbox.insert(pos+1, text)
+            listbox.selection_set(pos+1)
 
-    def execute(listbox):
+
+    def execute_merge(listbox):
+        '''begin the mergeing process'''
         output_file = asksaveasfilename(parent=mergewindow, filetypes=[('PDF Files', '.pdf')])
         merger = PdfFileMerger(strict=FALSE)
         for file_name in enumerate(listbox.get(0, END)):
@@ -31,10 +49,10 @@ def merge():
     maxlength = 0
     mergewindow.geometry('500x300')
     files = askopenfilenames(parent=mergewindow, filetypes=[("PDF Files", ".pdf")])
+    listbox = Listbox(mergewindow, selectmode=EXTENDED)
     button_up = Button(mergewindow, text='Move Up', command=lambda: move_up(listbox))
     button_down = Button(mergewindow, text='Move Down', command=lambda: move_down(listbox))
-    button_merge = Button(mergewindow, text='Merge', command=lambda: execute(listbox))
-    listbox = Listbox(mergewindow, selectmode=EXTENDED)
+    button_merge = Button(mergewindow, text='Merge', command=lambda: execute_merge(listbox))
     for item in files:
         listbox.insert(END, item)
         if len(item) > maxlength:
@@ -49,7 +67,31 @@ def merge():
 
 def split():
     '''Split function'''
-    print("split")
+
+    def execute_split(file, pages):
+        '''begin splitting process'''
+        fileout = asksaveasfilename(parent=splitwindow, filetypes=[('PDF Files', '.pdf')])
+        inputfile = PdfFileReader(open(file, 'rb'))
+        for i in pages:
+            outputfile = PdfFileWriter()
+            outputfile.addPage(inputfile.getPage(pages[i]))
+            outputfilename = (fileout
+            with open()
+
+    splitwindow = Tk()
+    splitwindow.geometry('500x300')
+    file = askopenfilename(parent=splitwindow, filetypes=[("PDF Files", ".pdf")])
+    listbox = Listbox(splitwindow, selectmode=EXTENDED)
+    pages = []
+    button_split = Button(splitwindow, text='Merge', command=lambda: execute_split(file, lambda: pages))
+    button_selectall = Button(splitwindow, text='Merge', \
+                                command=lambda: listbox.selection_set(0, END))
+    button_selectnone = Button(splitwindow, text='Merge', \
+                                command=lambda: listbox.selection_clear(0, END))
+    button_split.pack()
+    button_selectall.pack()
+    button_selectnone.pack()
+    splitwindow.mainloop()
 
 
 def reorder():
